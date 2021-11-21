cb402bf902a87e598ead2c9e86c2b7baaf2abca6
Takashi Hakomori <tamorin128@gmail.com>
2021-11-21 12:58:08 +0900

    test : add unit test for stack

--- test/budget-slack-notifier.test.ts
+++ test/budget-slack-notifier.test.ts
@@ -1,13 +1,126 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as BudgetSlackNotifier from '../lib/budget-slack-notifier-stack';
+import {
+  expect as expectCDK,
+  haveResourceLike,
+  countResources
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as BudgetSlackNotifier from "../lib/budget-slack-notifier-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new BudgetSlackNotifier.BudgetSlackNotifierStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("BudgetSlackNotifierStack", () => {
+  const app = new cdk.App();
+
+  // WHEN
+  const testProps = {
+    slackChannelId: "dummyChannel",
+    slackWorkspaceId: "dummyWorkspace",
+    unit: "USD",
+    amount: 10,
+    threshold: 100,
+    thresholdType: "PERCENTAGE",
+    timeUnit: "MONTHLY"
+  };
+  const stack = new BudgetSlackNotifier.BudgetSlackNotifierStack(
+    app,
+    "TestBudgetSlackNotifierStack",
+    testProps
+  );
+
+  // THEN
+  /* SNS */
+  expectCDK(stack).to(countResources("AWS::SNS::Topic", 1));
+  expectCDK(stack).to(
+    haveResourceLike("AWS::SNS::Topic", {
+      TopicName: "budget-slack-notifier-topic"
+    })
+  );
+  expectCDK(stack).to(
+    haveResourceLike("AWS::SNS::TopicPolicy", {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: "SNS:Publish",
+            Effect: "Allow",
+            Principal: {
+              Service: "budgets.amazonaws.com"
+            }
+          }
+        ]
+      }
+    })
+  );
+
+  /* Chatbot */
+  expectCDK(stack).to(countResources("AWS::IAM::Role", 1));
+  expectCDK(stack).to(
+    haveResourceLike("AWS::IAM::Role", {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Action: "sts:AssumeRole",
+            Effect: "Allow",
+            Principal: {
+              Service: "sns.amazonaws.com"
+            }
+          }
+        ]
+      }
+    })
+  );
+
+  expectCDK(stack).to(countResources("AWS::IAM::Policy", 1));
+  expectCDK(stack).to(
+    haveResourceLike("AWS::IAM::Policy", {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: "cloudwatch:*",
+            Effect: "Allow",
+            Resource: "*"
+          }
+        ]
+      }
+    })
+  );
+
+  expectCDK(stack).to(
+    countResources("AWS::Chatbot::SlackChannelConfiguration", 1)
+  );
+  expectCDK(stack).to(
+    haveResourceLike("AWS::Chatbot::SlackChannelConfiguration", {
+      ConfigurationName: "budget-slack-notifier-channel-config",
+      LoggingLevel: "ERROR",
+      SlackChannelId: testProps.slackChannelId,
+      SlackWorkspaceId: testProps.slackWorkspaceId
+    })
+  );
+
+  /* Budget */
+  expectCDK(stack).to(countResources("AWS::Budgets::Budget", 1));
+  expectCDK(stack).to(
+    haveResourceLike("AWS::Budgets::Budget", {
+      Budget: {
+        BudgetLimit: {
+          Amount: testProps.amount,
+          Unit: testProps.unit
+        },
+        BudgetType: "COST",
+        TimeUnit: testProps.timeUnit
+      },
+      NotificationsWithSubscribers: [
+        {
+          Notification: {
+            ComparisonOperator: "GREATER_THAN",
+            NotificationType: "ACTUAL",
+            Threshold: testProps.threshold,
+            ThresholdType: testProps.thresholdType
+          },
+          Subscribers: [
+            {
+              SubscriptionType: "SNS"
+            }
+          ]
+        }
+      ]
+    })
+  );
 });
